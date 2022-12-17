7e7cb5bccc2df216c1eeaf2e52ecb7cdf6eacd0d
fluorjo <fluorjo@naver.com>
2022-12-17 18:58:03 +0900

    4.3.Named Constructor

--- 4.2.Named Constructor Parameters.dart
+++ 4.2.Named Constructor Parameters.dart
@@ -1,29 +1,24 @@
-class Player{
-  final String name ;
-  //final 선언해주면 바꿀 수 없게 됨. 
+class Player {
+  final String name;
+  //final 선언해주면 바꿀 수 없게 됨.
   int xp;
   String team;
   int age;
-  
-  Player({
-    required this.name,
-    required this.xp,
-    required this.team,
-    required this.age
-    });
-    
-  void sayHello(){
+
+  Player(
+      {
+      //이 부분을 named constructor parameter라고 함.
+      required this.name,
+      required this.xp,
+      required this.team,
+      required this.age});
+
+  void sayHello() {
     print('Hi my name is $name');
-    
   }
 }
 
 void main() {
-  var player=Player(
-    name:'aa',
-    xp:100,
-    team:'dd',
-    age:88);
+  var player = Player(name: 'aa', xp: 100, team: 'dd', age: 88);
   player.sayHello();
-
 }
